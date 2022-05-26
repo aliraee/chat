3bd5f3ea280a0ce84cc10fc518b7967eaba91bb3
ali <aliraee1@yahoo.com>
2022-05-26 16:12:25 -0400

    add echo_image view

--- chat_system/echo/views.py
+++ chat_system/echo/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request,'echo/index.html')
+
+def echo_image(request):
+    return render(request,'echo/echo_image.html')

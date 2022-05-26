7e1b10b9ee5ff99f813146b8b426aba12a94d433
ali <aliraee1@yahoo.com>
2022-05-26 15:27:31 -0400

    add essential routing file

--- chat_system/chat_system/routing.py
+++ chat_system/chat_system/routing.py
@@ -1,4 +1,12 @@
-from email.mime import application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-application = ProtocolTypeRouter({})
+from echo import routing as echo_routing
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            echo_routing.websocket_urlpatterns
+        )
+    )
+})

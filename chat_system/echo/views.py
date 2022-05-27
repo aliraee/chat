aabbf9d302ca075cf631828a4c3094ddd5898655
ali <aliraee1@yahoo.com>
2022-05-27 02:56:17 -0400

    add join_chat view

--- chat_system/echo/views.py
+++ chat_system/echo/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from django.utils.safestring import mark_safe
+import json
 
 
 def index(request):
-    return render(request,'echo/index.html')
+    return render(request, 'echo/index.html')
+
 
 def echo_image(request):
-    return render(request,'echo/echo_image.html')
+    return render(request, 'echo/echo_image.html')
+
+def join_chat(request,username):
+    return render(request,'echo/join_chat',{'username':mark_safe(json.dumps(username))})

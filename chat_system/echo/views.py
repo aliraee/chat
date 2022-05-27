c1b373728d6d1ebb60bc9d344f7b8335546d29cd
ali <aliraee1@yahoo.com>
2022-05-27 03:53:45 -0400

    add chat view

--- chat_system/echo/views.py
+++ chat_system/echo/views.py
@@ -11,4 +11,4 @@ def echo_image(request):
     return render(request, 'echo/echo_image.html')
 
 def join_chat(request,username):
-    return render(request,'echo/join_chat',{'username':mark_safe(json.dumps(username))})
+    return render(request,'echo/join_chat.html',{'username_json':mark_safe(json.dumps(username))})
